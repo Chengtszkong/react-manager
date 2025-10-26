e3a806acda9cb720c449ee89b4ac2dcae8368f6b
Chengtszkong <chengtszkong.developer@outlook.com>
2025-10-26 23:18:44 +0800

    开发siderMenu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styles from './index.module.scss'
+import logoPng from '@/assets/images/logo.png'
+import { Menu } from 'antd'
+import {
+  DesktopOutlined,
+  SettingOutlined,
+  TeamOutlined
+} from '@ant-design/icons'
+import type { ItemType, MenuItemType } from 'antd/es/menu/interface'
+import { useNavigate } from 'react-router-dom'
+
+const items: ItemType<MenuItemType>[] = [
+  {
+    key: '1',
+    icon: React.createElement(DesktopOutlined),
+    label: '工作台'
+  },
+  {
+    key: '2',
+    icon: React.createElement(SettingOutlined),
+    label: '系统管理',
+    children: [
+      {
+        key: '3',
+        icon: React.createElement(TeamOutlined),
+        label: '用户管理'
+      }
+    ]
+  }
+]
+
+const SiderMenu: React.FC = () => {
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <div className={styles.logo} onClick={() => navigate('/welcome')}>
+        <img src={logoPng} alt='logo' />
+        <span>货拉拉</span>
+      </div>
+      <Menu
+        theme='dark'
+        mode='inline'
+        defaultSelectedKeys={['1']}
+        items={items}
+      />
+    </>
+  )
+}
+
+export default SiderMenu
